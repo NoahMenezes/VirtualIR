d5c47d7c9a61ccbbf7207b5862aacb3fafb54f20
NoahMenezes <2006noahmenezes@gmail.com>
2025-09-27 11:39:50 +0530

    HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
 
-const HeroSection = () => {
-  return (
-    <div>
-      
+const HeroSection = () => (
+    <div className="hero-section">
+        <h1 className="hero-title">
+            Accelerate your
+            <span className="gradient-text">
+                {' '}coding workflow.
+            </span>
+        </h1>
+        <p className="hero-subtitle">
+            Track the performance of your VR apps and gain insights into user behavior. Empower your creativity and bring your ideas to life with our intuitive developer tools.
+        </p>
     </div>
-  )
-}
+);
 
-export default HeroSection
+export default HeroSection;
