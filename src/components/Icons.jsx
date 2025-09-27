6c3a094c885cca238db2be05450866a43280cbc7
NoahMenezes <2006noahmenezes@gmail.com>
2025-09-27 11:39:00 +0530

    icons.jsx

---
+++ src/components/Icons.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const MenuIcon = () => (
+    <svg className="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-8 6h8" />
+    </svg>
+);
+
+export const CloseIcon = () => (
+    <svg className="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+    </svg>
+);
+
+export const CheckIcon = () => (
+    <svg className="icon check-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path></svg>
+);
